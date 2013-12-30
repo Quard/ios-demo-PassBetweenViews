97908f2e252a336ac690b81a55a9c19802e9f632
Vadym Zakovinko <vadym@zakovinko.com>
2013-12-30 16:32:20 +0200

    Initial Commit

---
+++ PassBetweenViews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PassBetweenViews
+//
+//  Created by Vadym Zakovinko on 12/30/13.
+//  Copyright (c) 2013 Vadym Zakovinko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PassBetweenViewsTests/PassBetweenViewsTests.m
@@ -0,0 +1,34 @@
+//
+//  PassBetweenViewsTests.m
+//  PassBetweenViewsTests
+//
+//  Created by Vadym Zakovinko on 12/30/13.
+//  Copyright (c) 2013 Vadym Zakovinko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PassBetweenViewsTests : XCTestCase
+
+@end
+
+@implementation PassBetweenViewsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
